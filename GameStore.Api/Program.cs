e381d1440df5df393df6757ad557cc36529ae6ee
jdelobel5987 <delobel.julien@outlook.com>
2026-03-16 12:08:09 +0100

    feat(database): AddGameStoreDb method
    register the app context with options to seed the Genre table if it is empty. Called within Program.cs

--- GameStore.Api/Program.cs
+++ GameStore.Api/Program.cs
@@ -1,5 +1,6 @@
 using GameStore.Api.Data;
 using GameStore.Api.Endpoints;
+using Microsoft.AspNetCore.Http.Connections;
 using System.Reflection; // pour inclure les commentaires XML dans Swagger
 
 var builder = WebApplication.CreateBuilder(args);
@@ -21,11 +22,8 @@ builder.Services.AddSwaggerGen(options =>   // ok sans options; ces options perm
 // utiliser les annotations de validation des DTOs
 builder.Services.AddValidation();
 
-// Define connection string for SQLite database
-var connString = "Data Source=GameStore.db"; // Beware: the string syntax depends on the DB provider (here SQLite)
-
-// Register GameStoreContext with the connection string
-builder.Services.AddSqlite<GameStoreContext>(connString);
+// Ajouter le contexte de données
+builder.AddGameStoreDb(); 
 
 var app = builder.Build();
 
