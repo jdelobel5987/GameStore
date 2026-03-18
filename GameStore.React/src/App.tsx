a2641ebd9e8b67d84d03ece058f7d5d230ade5ec
jdelobel5987 <delobel.julien@outlook.com>
2026-03-18 14:09:07 +0100

    feat(front): initialise the project with Vite
    Create the GameStore.React project with vite
    Cleanup the files
    Add basic file structure

---
+++ GameStore.React/src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+        <div>
+          <h1>GameStore API</h1>
+          <p>Welcome to the GameStore API!</p>
+          <p>Interface to manage game data coming soon!...</p>
+        </div>
+    </>
+  )
+}
+
+export default App
